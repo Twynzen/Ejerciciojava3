065aec1ab6a18b7d8c4b17f16a534c8ed2c99ab2
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-05-12 22:30:43 -0500

    Add files via upload

---
+++ arreglos3/src/arreglos3/Arreglos3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos3;
+
+/**
+ *
+ * @author LuisF
+ */
+public class Arreglos3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] nombres ={"Daniel", "Ale", "Wilson", "Michael","paco","lars","drogoz","khan"};
+        
+        for(String i: nombres){
+            System.out.println(i);        //bucle for each o mejorado
+        }
+        
+    }
+    
+}
